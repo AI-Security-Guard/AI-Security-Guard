0e70ecf3f7b3c078bdc9c58e490a253791aa8608
ehdgus5178 <163105838+ehdgus5178@users.noreply.github.com>
2025-05-10 19:19:17 +0900

    App.js파일에서 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './page/MainPage/MainPage.js';
+import LoginPage from './page/LoginPage/LoginPage.js';
+import DetailPage from './page/DetailPage/DetailPage.js';
 function App() {
     return (
         <>
             <Router>
                 <Routes>
                     <Route path='/' element={<MainPage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/Detail' element={<DetailPage />} />
                 </Routes>
             </Router>
         </>
